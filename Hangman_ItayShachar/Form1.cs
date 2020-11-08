65dc85a11c5627633fb8ad1033dc0dc12a321f04
איתי שחר <itayshachar04@gmail.com>
2020-11-08 13:02:57 +0200

    Add project files.

---
+++ Hangman_ItayShachar/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman_ItayShachar
+{
+    public partial class Form1 : Form
+    {
+        private int m_CurrentLabelLetter = 1;
+        private string m_WordToGuess;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            string buttonText = (sender as Button).Text;
+
+            LabelsBox.Controls["label" + m_CurrentLabelLetter].Text = buttonText;
+
+            m_CurrentLabelLetter++;
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (m_CurrentLabelLetter > 1)
+            {
+                LabelsBox.Controls["label" + (m_CurrentLabelLetter - 1)].Text = "_";
+
+                m_CurrentLabelLetter--;
+            }
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            Label curLabel;
+            for (int i = 1; i <= 5; i++)
+            {
+                curLabel = LabelsBox.Controls["label" + i] as Label;
+                m_WordToGuess += curLabel.Text;
+                curLabel.Text = "_";
+
+                DeleteButton.Visible = false;
+                OkButton.Visible = false;
+
+            }
+        }
+    }
+}
